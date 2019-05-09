42927eb6b12b8a1ebf11452282c944b3b021e1bd
Samiya Akhtar <saakhta@microsoft.com>
2019-05-09 12:38:08 -0700

    Test message in ui

--- ui/src/components/Home.tsx
+++ ui/src/components/Home.tsx
@@ -31,6 +31,8 @@ export class Home extends React.Component<IHomeProps, IHomeState> {
                 See <a href='https://datasets.imdbws.com/'> here for downloadable IMDB datasets</a>.
                 The datasets include 8.9 million people, 5.3 million movies and 30 million relationships between them.
 
+                This is a test.
+
                 <br />
                 <br />
 
